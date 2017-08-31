e125c9665fc22511493256621fe216f3525eef89
Henry Goldwire <henry.goldwire@gmail.com>
2017-08-31 18:06:11 -0400

    moving on to chapter 5

--- answers/src/main/scala/fpinscala/errorhandling/Option.scala
+++ answers/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,7 +1,7 @@
 package fpinscala.errorhandling
 
 //hide std library `Option` and `Either`, since we are writing our own in this chapter
-import scala.{Option => _, Either => _, _}
+import scala.{Either => _, Option => _, _}
 
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
@@ -9,7 +9,7 @@ sealed trait Option[+A] {
     case Some(a) => Some(f(a))
   }
 
-  def getOrElse[B>:A](default: => B): B = this match {
+  def getOrElse[B >: A](default: => B): B = this match {
     case None => default
     case Some(a) => a
   }
@@ -25,13 +25,13 @@ sealed trait Option[+A] {
     case Some(a) => f(a)
   }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] =
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
     this map (Some(_)) getOrElse ob
 
   /*
   Again, we can implement this with explicit pattern matching.
   */
-  def orElse_1[B>:A](ob: => Option[B]): Option[B] = this match {
+  def orElse_1[B >: A](ob: => Option[B]): Option[B] = this match {
     case None => ob
     case _ => this
   }
@@ -40,13 +40,16 @@ sealed trait Option[+A] {
     case Some(a) if f(a) => this
     case _ => None
   }
+
   /*
   This can also be defined in terms of `flatMap`.
   */
   def filter_1(f: A => Boolean): Option[A] =
     flatMap(a => if (f(a)) Some(a) else None)
 }
+
 case class Some[+A](get: A) extends Option[A]
+
 case object None extends Option[Nothing]
 
 object Option {
@@ -59,7 +62,9 @@ object Option {
     }
     // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern
     // that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
-    catch { case e: Exception => 43 }
+    catch {
+      case e: Exception => 43
+    }
   }
 
   def failingFn2(i: Int): Int = {
@@ -68,7 +73,9 @@ object Option {
       // A thrown Exception can be given any type; here we're annotating it with the type `Int`
       x + ((throw new Exception("fail!")): Int)
     }
-    catch { case e: Exception => 43 }
+    catch {
+      case e: Exception => 43
+    }
   }
 
   def mean(xs: Seq[Double]): Option[Double] =
@@ -80,8 +87,8 @@ object Option {
 
   // a bit later in the chapter we'll learn nicer syntax for
   // writing functions like this
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
-    a flatMap (aa => b map (bb => f(aa, bb)))
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+  a flatMap (aa => b map (bb => f(aa, bb)))
 
   /*
   Here's an explicit recursive version:
@@ -91,22 +98,23 @@ object Option {
       case Nil => Some(Nil)
       case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
     }
+
   /*
   It can also be implemented using `foldRight` and `map2`. The type annotation on `foldRight` is needed here; otherwise
   Scala wrongly infers the result type of the fold as `Some[Nil.type]` and reports a type error (try it!). This is an
   unfortunate consequence of Scala using subtyping to encode algebraic data types.
   */
   def sequence_1[A](a: List[Option[A]]): Option[List[A]] =
-    a.foldRight[Option[List[A]]](Some(Nil))((x,y) => map2(x,y)(_ :: _))
+    a.foldRight[Option[List[A]]](Some(Nil))((x, y) => map2(x, y)(_ :: _))
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
     a match {
       case Nil => Some(Nil)
-      case h::t => map2(f(h), traverse(t)(f))(_ :: _)
+      case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
     }
 
   def traverse_1[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
-    a.foldRight[Option[List[B]]](Some(Nil))((h,t) => map2(f(h),t)(_ :: _))
+    a.foldRight[Option[List[B]]](Some(Nil))((h, t) => map2(f(h), t)(_ :: _))
 
   def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] =
     traverse(a)(x => x)
